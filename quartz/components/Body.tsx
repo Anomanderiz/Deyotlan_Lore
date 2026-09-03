1fd374aec71a4b91dc1fc8bb3e3cc714312fd04e
Anomanderiz <91763965+Anomanderiz@users.noreply.github.com>
2026-09-03 00:36:16 -0700

    Add responsive mobile topo backdrop layer

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,7 +1,14 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
-  return <div id="quartz-body">{children}</div>
+  return (
+    <div id="quartz-body">
+      <div class="mobile-topo-backdrop" aria-hidden="true">
+        <div class="mobile-topo-backdrop__image" />
+      </div>
+      {children}
+    </div>
+  )
 }
 
 export default (() => Body) satisfies QuartzComponentConstructor
